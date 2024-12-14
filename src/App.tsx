331093d6d571ecf7294601deb21f9fb32223bc2c
Ajay Yadav <yajay04112000@gmail.com>
2024-12-14 09:16:01 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div className="page-wrapper">
+      <div className="content-wrapper"> helo Ajay</div>
+      <div className="content-wrapper"> helo Ajay1</div>
+    </div>
+  );
+}
+
+export default App;
