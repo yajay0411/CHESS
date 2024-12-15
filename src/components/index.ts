9121386707f56a5326eff0e76014aabf092701b7
Ajay Yadav <yajay04112000@gmail.com>
2024-12-15 15:58:40 +0530

    fix: Import casing fix

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,3 @@
 export { default as AllGamesListing } from './all_games_listing/AllGamesListing'
 export { default as GameCard } from './game_card/GameCard'
-export { default as ToggleMode } from './toggle_mode/toggleMode'
+export { default as ToggleMode } from './toggle_mode/ToggleMode'
